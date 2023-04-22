6ed382d3209f40f24d2d95d2ece7fe46f67b5bc0
pavanh <pavanhdsce@gmail.com>
2023-04-22 21:35:49 +0530

    new utility: email service added

---
+++ utilities/email.js
@@ -0,0 +1,25 @@
+import { createTransport } from 'nodemailer';
+
+const sendEmail = async (options) => {
+  //1. Create a transporter
+var transport = createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+  //2. Define email options
+  const mailOptions = {
+    from: 'pavan h <ticketmaster@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+  //3. send email with nodemailer
+  await transport.sendMail(mailOptions);
+};
+
+export default sendEmail;
