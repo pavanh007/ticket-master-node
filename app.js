b4cda5225350ff5570480b3c5269a55bd25858e0
pavanh <pavanhdsce@gmail.com>
2023-04-22 21:34:51 +0530

    Add middlewares to application

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
-const express = require('express');
+import express from 'express';
 const app = express();
+import morgan from 'morgan'; //logger
+import helmet from 'helmet'; //add header to request
+import  mongoSanitize from 'express-mongo-sanitize'; //to remove the sql injection attack
+import rateLimit from 'express-rate-limit'; //limit the request
+import appError from './utilities/appError.js';
+import userRouter from './routes/userRoutes.js';
+import cinemaRouter from './routes/movieRoutes.js';
+import globalErrorHandler from './controllers/errorController.js';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+//NOTE - middleware
+
+//Security HTTP headers
+app.use(helmet());
+
+//Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+// app.use(xss());
+
+//Development logging configuration
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('tiny'));
+}
+
+//Testting middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.headers);
+  next();
 });
 
-module.exports = app;
+//Setting up the maximum request to server to do action
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 100, // means 1 hour
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+
+//3. ROUTES
+app.use('/v1/users', userRouter);
+app.use('/v1/cinemas', cinemaRouter);
+
+app.all('*', (req, res, next) => {
+  next(new appError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+app.use(globalErrorHandler);
+
+export default app;
 
