8390eb0cff6179af07cd8eab4565e8ca09432288
pavanh <pavanhdsce@gmail.com>
2023-05-11 13:46:43 +0530

    all routes added

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ import morgan from 'morgan'; //logger
 import helmet from 'helmet'; //add header to request
 import  mongoSanitize from 'express-mongo-sanitize'; //to remove the sql injection attack
 import rateLimit from 'express-rate-limit'; //limit the request
+import bodyParser from 'body-parser';
 import appError from './utilities/appError.js';
 import userRouter from './routes/userRoutes.js';
-import cinemaRouter from './routes/movieRoutes.js';
-import globalErrorHandler from './controllers/errorController.js';
+import cityRouter from './routes/cityRoutes.js';
+import movieRouter from './routes/movieRoutes.js';
+import cinemaRouter from './routes/cinemaRoutes.js';
+import showRouter from './routes/cinemaRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
+import hallRouter from './routes/hallRoutes.js';
+import cinemaSeatRouter from './routes/cinemaSeatRoutes.js';
+import showSeatRouter from './routes/showSeatRoutes.js';
+
+// import globalErrorHandler from './controllers/errorController.js';
 
 //NOTE - middleware
 
@@ -22,6 +31,8 @@ app.use(mongoSanitize());
 
 //Data sanitization against XSS
 // app.use(xss());
+
+app.use(bodyParser.json());
 
 //Development logging configuration
 if (process.env.NODE_ENV === 'development') {
@@ -44,13 +55,22 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 //3. ROUTES
+// app.use('/v1/booking', bookingRouter);
 app.use('/v1/users', userRouter);
+app.use('/v1/cities', cityRouter);
+app.use('/v1/movies', movieRouter);
 app.use('/v1/cinemas', cinemaRouter);
+app.use('/v1/show', showRouter);
+app.use('/v1/hall', hallRouter);
+app.use('/v1/booking', bookingRouter);
+app.use('/v1/cinema-seat', cinemaSeatRouter);
+app.use('/v1/show-seat', showSeatRouter);
 
 app.all('*', (req, res, next) => {
   next(new appError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-app.use(globalErrorHandler);
+// app.use(globalErrorHandler);
 
 export default app;
+
 
